81ba7a28a4c28ff0500b086b0ef52a350ea75a91
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2020-12-10 10:00:32 +0530

    Application Architectire Review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // selector: 'app-main',
+  // selector: '[app-main]',
+  selector: '.app-main',
+  // templateUrl: './app.component.html'
+  // template:"<h1>Inline template example</h1><h2>Inline template Line 2</h2>"+
+  // "<h2>Inline template Line 3</h2>"
+  // template:`
+  // <h1>Inline template example</h1>
+  // <h2>Inline template Line 2</h2>
+  // <h2>Inline template Line 3</h2>
+  // `
+  templateUrl:"./app.component.html"
 })
-export class AppComponent {
-  title = 'December2020App';
-}
+export class AppComponent { }
