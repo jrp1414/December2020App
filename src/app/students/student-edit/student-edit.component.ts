789d12302a75dfa9e6cba06d46ccab18beabf05e
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2021-01-12 09:56:08 +0530

    RF Examples 5

--- src/app/students/student-edit/student-edit.component.ts
+++ src/app/students/student-edit/student-edit.component.ts
@@ -9,9 +9,17 @@ import { FormArray, FormBuilder, FormControl, FormControlName, FormGroup } from 
 export class StudentEditComponent implements OnInit {
 
   studentEditForm: FormGroup;
-  hobbies:FormArray = this.fb.array([]);
-
-  constructor(private fb:FormBuilder) { }
+  hobbies: FormArray = this.fb.array([]);
+  addresses: FormArray = this.fb.array([
+    this.fb.group({
+      AddLine1: this.fb.control(""),
+      AddLine2: this.fb.control(""),
+      AddLine3: this.fb.control(""),
+      City: this.fb.control(""),
+      State: this.fb.control("")
+    })
+  ]);
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.studentEditForm = this.fb.group({
@@ -20,14 +28,8 @@ export class StudentEditComponent implements OnInit {
       MobileNo: this.fb.control(""),
       EmailId: this.fb.control(""),
       NotificationType: this.fb.control(""),
-      Address: this.fb.group({
-        AddLine1: this.fb.control(""),
-        AddLine2: this.fb.control(""),
-        AddLine3: this.fb.control(""),
-        City: this.fb.control(""),
-        State: this.fb.control("")
-      }),
-      Hobbies:this.hobbies
+      Addresses: this.addresses,
+      Hobbies: this.hobbies
     });
   }
 
@@ -36,8 +38,18 @@ export class StudentEditComponent implements OnInit {
     console.log(this.studentEditForm.value);
   }
 
-  AddHobby(){
+  AddHobby() {
     this.hobbies.push(new FormControl());
+  }
+
+  AddAddress() {
+    this.addresses.push(this.fb.group({
+      AddLine1: this.fb.control(""),
+      AddLine2: this.fb.control(""),
+      AddLine3: this.fb.control(""),
+      City: this.fb.control(""),
+      State: this.fb.control("")
+    }));
   }
 
 }
